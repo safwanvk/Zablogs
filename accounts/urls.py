f35aeed179e3027d1ce4fc2168f3742b27bf92a5
safwanvk <safwanvalakundil@gmail.com>
2020-07-03 11:42:10 +0530

    Set password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,9 @@ from accounts import views
 urlpatterns = [
     path('register', views.register_view, name='register'),
     path('', include('django.contrib.auth.urls'), name='login'),
-    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password-reset'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset-password'),
     path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(), name='password-reset-done'),
-    path('reset<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
     path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password-reset-complete'),
     path('', views.home, name='home')
 ]
--- login/settings.py
+++ login/settings.py
@@ -119,3 +119,12 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# SMTP congigration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_LIST_TLS = True
+EMAIL_HOST_USER = 'muhannedsafwan250@gmail.com'
+EMAIL_HOST_PASSWORD = '#zayn789'
