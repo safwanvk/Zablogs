83de544079b96b7fddc6ea332c3d454edd4c48c2
safwanvk <safwanvalakundil@gmail.com>
2020-07-03 09:47:11 +0530

    Set register view

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,8 @@
+
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
 class UserCreationForm(UserCreationForm):
-
-    class Meta:
-        models = User
-        fields = '__all__'
+    model = User
+    fields = '__all__'
