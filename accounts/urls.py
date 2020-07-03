ea32adacad6bf7dccdde261cbda457debe079370
safwanvk <safwanvalakundil@gmail.com>
2020-07-03 12:22:04 +0530

    Set templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,9 @@ from accounts import views
 urlpatterns = [
     path('register', views.register_view, name='register'),
     path('', include('django.contrib.auth.urls'), name='login'),
-    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='reset-password'),
-    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(), name='password-reset-done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
-    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password-reset-complete'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='reset-password'),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'), name='password-reset-done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'), name='password-reset-confirm'),
+    path('reset-password-done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'), name='password-reset-complete'),
     path('', views.home, name='home')
 ]
